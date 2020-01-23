104cbd5060323a79a4703733019a4e734c04b128
Annette Odingo <odingoannette@gmail.com>
2020-01-23 07:48:14 +0000

    create a helper method findById()

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,4 +4,7 @@ const db = knex(knexConfig.development);
 function find(){
     return db('schemes');
 }
+function findById(id){
+    return db('schemes').where({id: Number(id)})
+}
 module.exports = { find }
